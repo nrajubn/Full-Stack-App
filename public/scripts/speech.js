9716e0d556c6abaa3540d87535e2abf1620c836f
Avisakula123 <rohithreddyas000@gmail.com>
2021-03-18 21:22:53 -0500

    added speech.js

---
+++ public/scripts/speech.js
@@ -0,0 +1,7 @@
+export default [
+    {
+      status_message:
+        "Congratulations, you have reached the location",
+    },
+    { status1_message: "Sorry, you are outside the location" },
+  ];
