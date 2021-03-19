6c6bc7a0efda01bf32df872b814897974937004c
Avisakula123 <rohithreddyas000@gmail.com>
2021-03-18 21:20:34 -0500

    added locations

---
+++ public/scripts/init-locations.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        name: 'UPD',
+        type: 'quad',
+        coordinates: [
+            {
+                latitude: 40.35606,
+                longitude: -94.88205
+            },
+            {
+                latitude: 40.35606,
+                longitude: -94.88205
+            },
+            {
+                latitude: 40.35606,
+                longitude: -94.88205
+            },
+            {
+                latitude: 40.35606,
+                longitude: -94.88205
+            }
+        ]
+    },
+    
+    {
+        Name:'Colden Pond',
+        Latitude:40.35007, 
+        Longitude:-94.88305,
+    },
+{
+    Name: "B.D Owens Library",
+    Latitude: 40.35385,
+    Longitude: -94.88591,
+  },
+
+];
