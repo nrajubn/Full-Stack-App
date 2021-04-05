1587a7a4df5ec78627bfc1177e977426acb1c757
nrajubn <60019513+nrajubn@users.noreply.github.com>
2021-04-04 23:57:56 -0500

    updated file

--- bin/www.js
+++ bin/www.js
@@ -95,12 +95,20 @@ const onListening = async () => {
   try {
     const db = await require("../models/index")();
     await require("../util/seeder")(db);
+    updatePostgreSequences(db);
   } catch (err) {
     LOG.error(`ERROR with database:${err.message}`);
   }
   const addr = server.address();
   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
   LOG.info(`Listening on ${bind}`);
+};
+const updatePostgreSequences = async (db) => {
+  if (process.env !== "production") return;
+  const extistingLocations = await db.models.location.count();
+  await db.queryInterface.sequelize.query(
+    `ALTER SEQUENCE "locations_id_seq" RESTART WITH ${extistingLocations + 1}`
+  );
 };
 
 /**
