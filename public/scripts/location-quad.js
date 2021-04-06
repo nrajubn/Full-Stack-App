970981d5a76425cc3bf5dac270c456b2774c7053
Avisakula123 <rohithreddyas000@gmail.com>
2021-04-06 14:25:10 -0500

    updated files

--- public/scripts/location-quad.js
+++ public/scripts/location-quad.js
@@ -1,81 +1,115 @@
 function isValid(coordinates) {
-    let lat = coordinates.latitude;
-    let lon = coordinates.longitude;
-  
-    if (lat < -90 || lat > 90 || lon < -180 || lon > 180) return false;
-    else return true;
-  }
-  
-  function isValidDevice(device) {
-    let deviceCoordinates = {};
-    deviceCoordinates["latitude"] = device.coords.latitude;
-    deviceCoordinates["longitude"] = device.coords.longitude;
-  
-    if (isValid(deviceCoordinates)) return true;
-    else throw "Invalid Device";
-  }
-  
-  function isValidType(location) {
-    if (location.type === "quad") return true;
-    else throw "Invalid Location Type";
-  }
-  
-  function isValidCoordinates(coordinates) {
-    if (coordinates.length != 4) return false;
-  
-    coordinates.forEach(function (coordinate, index) {
-      if (!isValid(coordinate)) return false;
-    });
-  
+  let lat = coordinates.latitude;
+  let lon = coordinates.longitude;
+
+  if (lat < -90 || lat > 90 || lon < -180 || lon > 180) return false;
+  else return true;
+}
+
+function isValidDevice(device) {
+  let deviceCoordinates = {};
+  deviceCoordinates["latitude"] = device.coords.latitude;
+  deviceCoordinates["longitude"] = device.coords.longitude;
+
+  if (isValid(deviceCoordinates)) return true;
+  else throw "Invalid Device";
+}
+
+function isValidType(location) {
+  if (location.type === "quad") return true;
+  else throw "Invalid Location Type";
+}
+
+function isValidCoordinates(coordinates) {
+  if (coordinates.length != 4) return false;
+
+  coordinates.forEach(function (coordinate, index) {
+    if (!isValid(coordinate)) return false;
+  });
+
+  return true;
+}
+
+function isValidLocation(location) {
+  if (
+    location.name.length > 0 &&
+    isValidType(location) &&
+    isValidCoordinates(location.coordinates)
+  )
     return true;
-  }
-  
-  function isValidLocation(location) {
-    if (
-      location.name.length > 0 &&
+  else throw "Invalid Location";
+}
+
+function isValidArguments(device, location) {
+  if (device == null || location == null)
+    throw "Two valid arguments are needed";
+  else return true;
+}
+
+export function isInsideQuad(device, location) {
+  console.log(device,location);
+  try {
+    let checkValid =
+      isValidArguments(device, location) &&
+      isValidDevice(device) &&
       isValidType(location) &&
-      isValidCoordinates(location.coordinates)
-    )
-      return true;
-    else throw "Invalid Location";
-  }
-  
-  function isValidArguments(device, location) {
-    if (device == null || location == null)
-      throw "Two valid arguments are needed";
-    else return true;
-  }
-  
-  export function isInsideQuad(device, location) {
-    try {
-      let checkValid =
-        isValidArguments(device, location) &&
-        isValidDevice(device) &&
-        isValidType(location) &&
-        isValidLocation(location);
-      if (checkValid) {
-        let x = device.coords.latitude;
-        let y = device.coords.longitude;
-  
-        let inside = false;
-        let coordinates = location.coordinates;
-        for (
-          let i = 0, j = coordinates.length - 1;
-          i < coordinates.length;
-          j = i++
-        ) {
-          let xi = coordinates[i]["latitude"],
-            yi = coordinates[i]["longitude"];
-          let xj = coordinates[j]["latitude"],
-            yj = coordinates[j]["longitude"];
-  
-          let intersect =
-            yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
-          if (intersect) inside = !inside;
-        }
-        return inside;
+      isValidLocation(location);
+    if (checkValid) {
+      let [x,y]=[device.coords.latitude, device.coords.longitude];
+
+      let inside = false;
+      let coordinates = location.coordinates;
+      for (
+        let i = 0, j = coordinates.length - 1;
+        i < coordinates.length;
+        j = i++
+      ) {
+        let xi = coordinates[i]["latitude"],
+          yi = coordinates[i]["longitude"];
+        let xj = coordinates[j]["latitude"],
+          yj = coordinates[j]["longitude"];
+
+        let intersect =
+          yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+        if (intersect) inside = !inside;
       }
-    } catch (err) {
-      console.log("Exception: " + err);
+      return inside;
     }
+  } catch (err) {
+    console.log("Exception: " + err);
   }
+}
+/* Get directions towards the quad from the current location*/
+export function getLocationDirections(device, location) {
+  let directionArray = [];
+  try {
+    let checkValid =
+      isValidArguments(device, location) &&
+      isValidDevice(device) &&
+      isValidType(location) &&
+      isValidLocation(location);
+    if (checkValid) {
+      let [x, y] = [device.coords.latitude, device.coords.longitude];
+
+      const latitudes = location.coordinates
+        .map((item) => item.latitude)
+        .sort((a, b) => b - a);
+      const longitudes = location.coordinates
+        .map((item) => item.longitude)
+        .sort((a, b) => a - b);
+
+      if (x > latitudes[0] || x > latitudes[1]) directionArray.push("South");
+      else if (x < latitudes[3] || x < latitudes[4])
+        directionArray.push("North");
+
+      if (y < longitudes[0] || y < longitudes[1]) directionArray.push("East");
+      else if (y > longitudes[3] || y > longitudes[4])
+        directionArray.push("West");
+
+      return directionArray;
+    }
+  } catch (err) {
+    console.log("Exception: " + err);
+  }
+}
+
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,5 @@
 import locationsArray from "./init-locations.js";
-import { isInsideQuad } from "./location-quad.js";
+import { isInsideQuad,getLocationDirections } from "./location-quad.js";
 
 
 
@@ -18,17 +18,28 @@ colorElement2.addEventListener("click", onClickSquareBox2);
 colorElement2.addEventListener("touch", onClickSquareBox2);
 
 async function onClickSquareBox1() {
-  location = locationsArray[0];
-  let confirmation = "Your target location is " + location.name;
-  document.getElementById("status1").innerHTML = confirmation;
-  let utterance = new SpeechSynthesisUtterance(confirmation);
-  speechSynthesis.speak(utterance);
+ /* Ajax to get a random location from the database when the first box is clicked */	
+ $.ajax({	
+  type: "GET",	
+  url: "/location/get-a-random-location",	
+  success: function (coord) {	
+    location = {	
+      name: coord.locationName,	
+      type: "quad",	
+      coordinates: coord.coordinate,	
+    };	
+    let confirmation = "Your target location is " + location.name;	
+    colorElement1.innerHTML = confirmation;	
+    let utterance = new SpeechSynthesisUtterance(confirmation);	
+    speechSynthesis.speak(utterance);	
+  },	
+});
 }
-
 async function onClickSquareBox2() {
   device = await getLocation();
 
   let isInside = isInsideQuad(device, location);
+  console.log(isInside);
   let status;
   let speak;
   status = "Device Coordinates: " + "<br>";
@@ -37,10 +48,27 @@ async function onClickSquareBox2() {
   if (isInside) {
     status += "Congratulations!! You have reached Quest: " + location.name;
     speak = "Congratulations!! You have reached Quest: " + location.name;
-  } else {
-    status += "You haven't reached the quest";
-    speak = "You haven't reached the quest";
-  }
+  
+  Swal.fire({	
+    title: "Congratulations!",	
+    text: status,	
+    icon: "sucess",	
+    confirmButtonText: "Cool",	
+  });	
+    }
+    else {	
+      let directionsArray = getLocationDirections(device, location);	
+      let directionsString = ` Please head ${	
+        directionsArray.length === 2	
+          ? directionsArray[0] + " " + directionsArray[1]	
+          : directionsArray[0]	
+      }.`;	
+    
+      status += "You haven't reached the quest yet.";	
+      status += directionsString;	
+      speak = "You haven't reached the quest yet. " + directionsString;	
+    }
+  
   document.getElementById("status2").innerHTML = status;
   let utterance = new SpeechSynthesisUtterance(speak);
   speechSynthesis.speak(utterance);
